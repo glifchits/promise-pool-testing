7bee5db0dd98dd6d07d21e6d0e2752a9f10eeaf1
George Lifchits <george.lifchits@gmail.com>
2016-05-18 14:32:48 -0400

    more promise-queue testing

--- main.js
+++ main.js
@@ -11,23 +11,18 @@ var url = 'http://localhost:8888/';
 const start = new Date();
 console.log('started', start);
 
-/*
-    return request.get(url).endAsync().then(res => callback(null, res)).catch(callback);
-*/
-
 Queue.configure(Promise); // use bluebird promise
 
 let q = new Queue(2 /* concurrent */, Infinity /* max in queue */);
 
 for (let i = 0; i < 10; i++) {
-    q.add(() => {
-        console.log('requested', i);
-        return request.get(url).endAsync()
-            .then(() => {
-                const end = new Date();
-                console.log(`${i} finished after ${((end-start)/1000).toFixed(2)} sec`);
-            });
+    let qAdd = q.add(() => (
+        request.get(url).endAsync().then(() => {
+            const end = new Date();
+            console.log(`${i} finished after ${((end-start)/1000).toFixed(2)} sec`);
+        })
+    ))
+    qAdd.then(function() {
+        console.log('qadd then\'d');
     });
 }
-
-
